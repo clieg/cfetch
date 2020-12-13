abfdf166fc9834ac799e0c30a4df4a30edb86039
Clint <egargoclint@protonmail.ch>
2020-12-13 20:16:43 +0800

    Updated: refactored some code

--- src/coffeetch.c
+++ src/coffeetch.c
@@ -1,79 +1,99 @@
 // coffeetch: a minimal CLI system information tool written in C.
 
 
+
+
+
 #include <stdio.h>
 
+// Text colours and weight
+#define NEWLINE = '\n'
+#define RESET "\x1b[0m"     // Reset
+#define TBOLD "\x1b[1m"     // Bold
+#define CBLACK "\x1b[30m"   // Black
+#define CRED "\x1b[31m"     // Red
+#define CGREEN "\x1b[32m"   // Green
+#define CYELLOW "\x1b[33m"  // Yellow
+#define CBLUE "\x1b[34m"    // Blue
+#define CMAGENTA "\x1b[35m" // Magenta
+#define CCYAN "\x1b[36m"    // Cyan
+#define CWHITE "\x1b[37m"   // White
+
 int day, hour, min, sec, ramused, ramtotal, dpkg, flatpak;
-char  user[50], host[50], os[50], model[50], modelversion[25], kernel[50], shell[25], cpu[50];
+char  user[50], host[50], os[50], model[50], modelversion[25], kernel[25], shell[25], cpu[50];
 
+// Detects the current user information
+// This detect get the username and the hostname
+void detectUser() {
+    FILE *detecuser = popen("echo $USER", "r");
+    FILE *detecthost = popen("cat /proc/sys/kernel/hostname", "r");
 
-// Get current user
-void getUser() {
-    FILE *userName = popen("echo $USER", "r");
-    FILE *userHost = popen("cat /proc/sys/kernel/hostname", "r");
-
-    fscanf(userName, "%s", user);
-    fscanf(userHost, "%s", host);
-    fclose(userName);
-    fclose(userHost);
+    fscanf(detecuser, "%s", user);
+    fscanf(detecthost, "%s", host);
+    fclose(detecuser);
+    fclose(detecthost);
 }
 
-// Get distro name
-void getDistro() {
-    FILE *distroName = popen("lsb_release -ds 2>/dev/null", "r");
+// Detects the OS name
+// This will detect the short description of the Linux Standard Base (LBS) release information
+void detectOS() {
+    FILE *detectos = popen("lsb_release -ds 2>/dev/null", "r");
 
-    fscanf(distroName, "%[^\n]s", os);
-    fclose(distroName);
-}
-
-
-// Get kernel
-void getKernel() {
-    FILE *pathKernel = popen("uname -rsm", "r");
-
-    fscanf(pathKernel, "%[^\n]s", kernel);
-    fclose(pathKernel);
+    fscanf(detectos, "%[^\n]s", os);
+    fclose(detectos);
 }
 
 
-// Get model information
-void getModel() {
-    FILE *productName = fopen("/sys/devices/virtual/dmi/id/product_name", "r");
-    FILE *productVersion = fopen("/sys/devices/virtual/dmi/id/product_version", "r");
+// Detects the kernel
+// This will detect the kernel's name, kernel-release, and machine hardware name
+void detectKernel() {
+    FILE *detectkernel = popen("uname -rsm", "r");
 
-    fscanf(productName, "%[^\n]s", model);
-    fscanf(productVersion, "%s", modelversion);
-    fclose(productName);
-    fclose(productVersion);
+    fscanf(detectkernel, "%[^\n]s", kernel);
+    fclose(detectkernel);
 }
 
 
-// Get shell information
-void getShell() {
-    FILE *shellpath = popen("echo $SHELL", "r");
+// Detects the model information
+// This detect get the machine's name and model information
+void detectModel() {
+    FILE *detectmodel = fopen("/sys/devices/virtual/dmi/id/product_name", "r");
+    FILE *detectmodelversion = fopen("/sys/devices/virtual/dmi/id/product_version", "r");
 
-    fscanf(shellpath, "%s", shell);
-    fclose(shellpath);
+    fscanf(detectmodel, "%[^\n]s", model);
+    fscanf(detectmodelversion, "%s", modelversion);
+    fclose(detectmodel);
+    fclose(detectmodelversion);
 }
 
 
-// Get packages
+// Detects the shell information
+// This will detect the shell that is being used
+void detectShell() {
+    FILE *detectshell = popen("echo $SHELL", "r");
+
+    fscanf(detectshell, "%s", shell);
+    fclose(detectshell);
+}
+
+
+// Detects the packages
 // Currently this only displays dpkg and flatpaks
-void getPackages() {
-    FILE *dpkgs = popen("dpkg-query -l | wc -l 2>/dev/null", "r");
-    FILE *flatpaks = popen("flatpak list | wc -l 2>/dev/null", "r");
+void detectPackages() {
+    FILE *detectdpkgs = popen("dpkg-query -l | wc -l 2>/dev/null", "r");
+    FILE *detectflatpaks = popen("flatpak list | wc -l 2>/dev/null", "r");
 
-    fscanf(dpkgs, "%d", &dpkg);
-    fscanf(flatpaks, "%d", &flatpak);
-    fclose(dpkgs);
-    fclose(flatpaks);
+    fscanf(detectdpkgs, "%d", &dpkg);
+    fscanf(detectflatpaks, "%d", &flatpak);
+    fclose(detectdpkgs);
+    fclose(detectflatpaks);
 }
 
 
-// Get CPU information
+// Detects the CPU information
 // lscpu | grep 'Model name:' | sed -r 's/Model name:\\s{1,}//
 // sed -r 's/Model name:\\s{1,}// -> This will remove the 'Model name:', only the CPU will be printed.
-void getCPU() {
+void detectCPU() {
     FILE *cpuinfo = popen("lscpu | grep 'Model name:' | sed -r 's/Model name:\\s{1,}//'", "r");
 
     fscanf(cpuinfo, "%[^\n]s", cpu);
@@ -81,8 +101,9 @@ void getCPU() {
 }
 
 
-// Get RAM information
-void getRAM() {
+// Detects the RAM information
+// This will detect the used RAM / the total RAM
+void detectRAM() {
     FILE *used = popen("vmstat -s -S M | grep ' used memory'", "r");
     FILE *total = popen("vmstat -s -S M | grep ' total memory'", "r");
 
@@ -93,8 +114,10 @@ void getRAM() {
 }
 
 
-// Get uptime information
-void getUptime() {
+// Detects the uptime information
+// This will detect the uptime of the machin
+// DDd, HHh, MMm
+void detectUptime() {
     FILE *pathUptime = fopen("/proc/uptime", "r");
 
     fscanf(pathUptime, "%d", &sec);
@@ -106,45 +129,37 @@ void getUptime() {
 }
 
 
-// Call all functions
+// Call all functions that detects the system information
 void init() {
-    getUser();
-    getDistro();
-    getModel();
-    getKernel();
-    getShell();
-    getPackages();
-    getCPU();
-    getRAM();
-    getUptime();
+    detectUser();
+    detectOS();
+    detectModel();
+    detectKernel();
+    detectShell();
+    detectPackages();
+    detectCPU();
+    detectRAM();
+    detectUptime();
 }
 
-/*
- *   Colour legend
- *   \x1b[30m - Black
- *   \x1b[31m - Red
- *   \x1b[32m - Green
- *   \x1b[33m - YelloW os:\x
- *   \x1b[34m - Blue
- *   \x1b[35m - Magenta
- *   \x1b[36m - Cyan
- *   \x1b[37m - White
- */
-int main(int argc, char *argv[]) {
+
+
+int main() {
     init();
 
     // Do not change these
+    // Changing these will only disportionate the printed system information
     // user, os, kernel, model, cpu, ram, shell, pkgs, uptime, pallate colour
-    printf("\n\x1b[1m \x1b[36mcoffeetch\x1b[0m: a minimal CLI system information tool written in C.\n\n");
-    printf("\x1b[1m        ___          \x1b[36muser\x1b[0m:       %s@%s\n", user, host);
-    printf("\x1b[1m       (.· |         \x1b[36mos\x1b[0m:         %s\n", os);
-    printf("\x1b[1m       (<> |         \x1b[36mkernel\x1b[0m:     %s\n", kernel);
-    printf("\x1b[1m      / __  \\        \x1b[36mmodel\x1b[0m:      %s %s\n", model, modelversion);
-    printf("\x1b[1m     ( /  \\ /|       \x1b[36mcpu\x1b[0m:        %s\n", cpu);
-    printf("\x1b[1m    _/\\ __)/_)       \x1b[36mram\x1b[0m:        %dM / %dM\n", ramused, ramtotal);
-    printf("\x1b[1m    \\|/-___\\|/       \x1b[36mshell\x1b[0m:      %s\n", shell);
-    printf("\x1b[1m                     \x1b[36mpkgs\x1b[0m:       %d(dpkg), %d(flatpak)\n", dpkg, flatpak);
-    printf("\x1b[1m\x1b[30m ██\x1b[31m██\x1b[32m██\x1b[33m██\x1b[34m██\x1b[35m██\x1b[36m██\x1b[37m██\x1b[0m    \x1b[1;36muptime\x1b[0m:     %dd, %dh, %dm\n", day, hour, min);
+    printf("%s%s coffeetch%s: a minimal CLI system information tool written in C.\n\n", TBOLD, CCYAN, RESET);
+    printf("%s         ___          %suser%s:      %s@%s\n", TBOLD, CCYAN, RESET, user, host);
+    printf("%s        (.· |         %sos%s:        %s\n", TBOLD, CCYAN, RESET, os);
+    printf("%s        (<> |         %skernel%s:    %s\n", TBOLD, CCYAN, RESET, kernel);
+    printf("%s       / __  \\        %smodel%s:     %s %s\n", TBOLD, CCYAN, RESET, model, modelversion);
+    printf("%s      ( /  \\ /|       %scpu%s:       %s\n", TBOLD, CCYAN, RESET, cpu);
+    printf("%s     _/\\ __)/_)       %sram %s:      %dM / %dM\n", TBOLD, CCYAN, RESET, ramused, ramtotal);
+    printf("%s     \\|/-___\\|/       %sshell%s:     %s\n", TBOLD, CCYAN, RESET, shell);
+    printf("%s                      %spkgs%s:      %d(dpkg), %d(flatpak)\n", TBOLD, CCYAN, RESET, dpkg, flatpak);
+    printf("%s %s ██%s██%s██%s██%s██%s██%s██%s██    %suptime%s:    %dd %dh %dm", TBOLD, CBLACK, CRED, CGREEN, CYELLOW, CBLUE, CMAGENTA, CCYAN, CWHITE, CCYAN, RESET, day, hour, min);
 
     printf("\n\n\n");
 
